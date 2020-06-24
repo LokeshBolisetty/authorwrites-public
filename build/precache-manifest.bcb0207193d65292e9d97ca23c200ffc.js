8bae5364df5224faa8911f91d418c30ed4d266b0
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-24 14:17:56 +0530

    Added build

---
+++ build/precache-manifest.bcb0207193d65292e9d97ca23c200ffc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7deb5926538d941e3da07e3b3db343",
+    "url": "/AuthorWrites/index.html"
+  },
+  {
+    "revision": "8a2a89d63a4719e5e1f2",
+    "url": "/AuthorWrites/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed189d857fa85d43dd8b",
+    "url": "/AuthorWrites/static/css/main.5601ab5f.chunk.css"
+  },
+  {
+    "revision": "8a2a89d63a4719e5e1f2",
+    "url": "/AuthorWrites/static/js/2.363d05c1.chunk.js"
+  },
+  {
+    "revision": "20451c1d098c44474cfe49e2f962af34",
+    "url": "/AuthorWrites/static/js/2.363d05c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed189d857fa85d43dd8b",
+    "url": "/AuthorWrites/static/js/main.f1823283.chunk.js"
+  },
+  {
+    "revision": "a609cd76c8d008135b44",
+    "url": "/AuthorWrites/static/js/runtime-main.8c211e5d.js"
+  },
+  {
+    "revision": "fcb3809bac6c7d2deee609933766749c",
+    "url": "/AuthorWrites/static/media/Wood Background.fcb3809b.jfif"
+  },
+  {
+    "revision": "3c9b5c3fc08781c531a79ab8ab6ad747",
+    "url": "/AuthorWrites/static/media/search image.3c9b5c3f.png"
+  }
+]);
